8227782debc4b2d081d71c40c56be134c852b6db
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-05-10 08:02:06 +0000

    Retrive öatest stock numbers form spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,6 +12,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
@@ -33,7 +35,6 @@ def get_sales_data():
     return sales_data
 
 
-
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -53,7 +54,6 @@ def validate_data(values):
     return True
 
 
-
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
@@ -64,6 +64,28 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for eac item type.
+
+    the surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicate wast
+    - Negative surplus indicates ectra made when stock was sold out-
+    """
+    print("Calculating surplus data....\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    pprint(stock_row)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
